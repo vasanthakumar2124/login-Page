54aa8e867c05f98b844546b79f60437ea44fe98f
vasanthakumar2124 <vasanthakumar2124@gmail.com>
2026-05-07 04:50:03 -0700

    changes did

--- accounts/accounts/urls.py
+++ accounts/accounts/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('users.urls')),
     path('user/',include('django.contrib.auth.urls'))
+
+    path('', lambda request: redirect('user/'))
 ]
